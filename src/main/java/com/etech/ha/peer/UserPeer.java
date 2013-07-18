557776e996539e7f2e70a8c7a5415e8623bc6f30
changer20001 <changer20001@sohu.com>
2013-07-18 17:24:12 +0800

    启用spring对hibernate的annotations配置。由此引入了2个jar：lib\hibernate-annotations-3.2.1.ga.jar 和 com.springsource.javax.persistence-1.0.0.jar， 并且Peer里的class都需要加上annotation，否则数据库操作无法正常进行

--- src/main/java/com/etech/ha/peer/UserPeer.java
+++ src/main/java/com/etech/ha/peer/UserPeer.java
@@ -2,19 +2,46 @@ package com.etech.ha.peer;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="t_user")
 public class UserPeer implements Serializable {
 	
 	private static final long serialVersionUID = 5349810873546597127L;
 	
+	@Id
+	@Column(name="id")
 	private Long id;
+	
+	@Column(name="empe_num")
 	private String empe_num;
+	
+	@Column(name="empe_name")
 	private String empe_name;
+	
+	@Column(name="pwd")
 	private String pwd;
+	
+	@Column(name="mobile")
 	private String mobile;
+	
+	@Column(name="tel_no")
 	private String tel_no;
+	
+	@Column(name="email")
 	private String email;
+	
+	@Column(name="dept_cd")
 	private String dept_cd;
+	
+	@Column(name="del_flg")
 	private int del_flg;
+	
 	public Long getId() {
 		return id;
 	}
