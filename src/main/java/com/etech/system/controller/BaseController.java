63457bccab232d84f5313ae1207496323bd66e6b
changer20001 <changer20001@sohu.com>
2013-08-11 12:09:52 +0800

    自动注入org.springframework.context.MessageSource

--- src/main/java/com/etech/system/controller/BaseController.java
+++ src/main/java/com/etech/system/controller/BaseController.java
@@ -1,15 +1,29 @@
 package com.etech.system.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
 import com.etech.ha.constants.HaConstants;
 import com.etech.system.bean.UserInfo;
 
 public class BaseController {
+	
+	@Autowired
+	protected MessageSource messageSource;
 	
 	public int accessRight(UserInfo userInfo, String action) {
 		
 		//TODO
 		
 		return HaConstants.ACCESS_OK;
+	}
+
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
 	}
 	
 }
