6cb8afd61a8574ca42e652f69da74dbbb88a3ea7
changer20001 <changer20001@sohu.com>
2013-08-29 21:55:33 +0800

    增加了空值选项

--- src/main/java/com/etech/ha/mst/controller/HolidayListController.java
+++ src/main/java/com/etech/ha/mst/controller/HolidayListController.java
@@ -1,5 +1,6 @@
 package com.etech.ha.mst.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,10 @@ public class HolidayListController extends BaseController {
 	@ModelAttribute("hldyOptions")
 	public List<HolidayPeer> initHldyOptions() {
 		List<HolidayPeer> list = hldySvc.listAll();
+		if (list==null) {
+			list = new ArrayList<HolidayPeer>();
+		}
+		list.add(0, new HolidayPeer());
 		return list;
 	}
 	
