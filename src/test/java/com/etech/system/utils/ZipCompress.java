39858bf39e25db24a6182734e8177ab5a417336e
changer20001 <changer20001@sohu.com>
2013-10-13 21:02:51 +0800

    zip例子

---
+++ src/test/java/com/etech/system/utils/ZipCompress.java
@@ -0,0 +1,39 @@
+package com.etech.system.utils;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipCompress {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		File file = new File("test.zip");
+		//file.setWritable(true);
+		//file.createNewFile();
+		FileOutputStream f = new FileOutputStream(file);
+		ZipOutputStream zos = new ZipOutputStream(f);
+		BufferedOutputStream out = new BufferedOutputStream(zos);
+		for (String arg :args) {
+			System.out.println("Writing file " + arg);
+			
+			BufferedReader in = new BufferedReader(new FileReader(arg));
+			zos.putNextEntry(new ZipEntry(arg));
+			int c;
+			while ((c=in.read())!=-1) {
+				out.write(c);
+			}
+			in.close();
+			out.flush();
+		}
+		out.close();
+	}
+
+}
